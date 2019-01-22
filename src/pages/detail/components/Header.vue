d2857ed19436584937f2bb7df9943d293cf6b83e
letty <chenxf@ngarihealth.com>
2019-01-22 15:58:49 +0800

    解绑全局事件

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -52,6 +52,9 @@ export default {
   },
   activated () {
     window.addEventListener('scroll', this.handleScroll)
+  },
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
